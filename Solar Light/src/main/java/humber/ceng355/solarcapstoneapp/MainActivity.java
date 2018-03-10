83d0fe03e748f7038374c288469e6284957b78f1
RaphaelNajera <31861631+RaphaelNajera@users.noreply.github.com>
2018-03-10 01:28:24 -0500

    Updated PV1
    Add the code for reading data from the firebase

--- Solar Light/src/main/java/humber/ceng355/solarcapstoneapp/MainActivity.java
+++ Solar Light/src/main/java/humber/ceng355/solarcapstoneapp/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void selectItem(int position) {
 
         //Navigation Drawer
--- Solar Light/src/main/java/humber/ceng355/solarcapstoneapp/SolarPV1.java
+++ Solar Light/src/main/java/humber/ceng355/solarcapstoneapp/SolarPV1.java
@@ -2,9 +2,19 @@ package humber.ceng355.solarcapstoneapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -16,15 +26,61 @@ public class SolarPV1 extends Fragment {
     public SolarPV1() {
     }
 
+    DatabaseReference myRef;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("PV1");
         View rootView = inflater.inflate(R.layout.fragment_solarpv1, container, false);
 
         //Connection to Firebase Database
 
 
         return rootView;
+    }
+
+    public void onStart(){
+        super.onStart();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+
+                for(DataSnapshot dss : dataSnapshot.getChildren()) {
+                    //Retrieving the data stored on the firebase
+                    String PV1_Date = dss.child("Date").getValue(String.class);
+                    String PV1_Power = dss.child("Power").getValue(String.class);
+                    String PV1_Daily = dss.child("Daily yield").getValue(String.class);
+                    String PV1_Total = dss.child("Total yield").getValue(String.class);
+                    //Declaring textviews
+                    TextView PV1DateValueTV = getActivity().findViewById(R.id.PV1_Date_Value);
+                    TextView PV1PowerValueTV = getActivity().findViewById(R.id.PV1_Power_Value);
+                    TextView PV1DailyValueTV = getActivity().findViewById(R.id.PV1_Dialyyield_Value);
+                    TextView PV1TotalValueTV = getActivity().findViewById(R.id.PV1_Totalyield_Value);
+
+
+                    //Display the Date, Current Power, Daily yield and Total yield that PV1 stored on the firebase
+                    PV1DateValueTV.setText(PV1_Date);
+                    PV1PowerValueTV.setText(PV1_Power);
+                    PV1DailyValueTV.setText(PV1_Daily);
+                    PV1TotalValueTV.setText(PV1_Total);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
 
 }
